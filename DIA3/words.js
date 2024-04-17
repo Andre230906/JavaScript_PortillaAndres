c0d87ac72f0890729630eb36756c56f473e5ee6b
Andre230906 <andresportillaart@gmail.com>
2024-04-16 20:28:03 -0500

    build: :construction: Codigo de lead
    Se suben los 3 codigos de entrenamiento de lead

---
+++ DIA3/words.js
@@ -0,0 +1,18 @@
+function replaceWords(dictionary, sentence) {
+    const successorToRoot = {};
+
+    for (const root of dictionary) {
+        const successor = root.slice(1);
+        if (!(successor in successorToRoot) || root.length < successorToRoot[successor].length) {
+            successorToRoot[successor] = root;
+        }
+    }
+
+    const words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        for (const successor in successorToRoot) {
+            if (word.endsWith(successor)) {
+                words[i] = successorToRoot[successor] + word.slice(successor.length);
+                break; // Stop after replacing with the shortest root
+            }
