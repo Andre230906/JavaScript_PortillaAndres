209312039c82b8daeef17a1fddb399e99874d733
Andre230906 <andresportillaart@gmail.com>
2024-04-23 08:35:16 -0500

    Ordenamiento de datos del personaje

--- DIA8/main.js
+++ DIA8/main.js
@@ -6,15 +6,32 @@ document.getElementById('apiForm').addEventListener('submit', function(e) {
     fetch(apiUrl + '/' + id)
         .then(response => response.json())
         .then(data => {
-            const formattedData = JSON.stringify(data, null, 4);
+            const template = `
+                <h2>${data.name}</h2>
+                <p><strong>Altura:</strong> ${data.height}</p>
+                <p><strong>Peso:</strong> ${data.mass}</p>
+                <p><strong>Color de pelo:</strong> ${data.hair_color}</p>
+                <p><strong>Color de piel:</strong> ${data.skin_color}</p>
+                <p><strong>Color de ojos:</strong> ${data.eye_color}</p>
+                <p><strong>Año de nacimiento:</strong> ${data.birth_year}</p>
+                <p><strong>Género:</strong> ${data.gender}</p>
+                <p><strong>Mundo:</strong> ${data.homeworld}</p>
+                <p><strong>Películas:</strong> ${data.films.join(', ')}</p>
+                <p><strong>Especies:</strong> ${data.species.join(', ')}</p>
+                <p><strong>Naves:</strong> ${data.starships.join(', ')}</p>
+                <p><strong>fecha de creacion:</strong> ${data.created}</p>
+                <p><strong>ultima feecha de edicion:</strong> ${data.edited}</p>
+                <p><strong>URL:</strong> ${data.url}</p>
+            `;
 
-            document.getElementById('result').innerHTML = `<pre>${formattedData}</pre>`;
+            document.getElementById('result').innerHTML = template;
         })
         .catch(error => {
-            console.error('Error al encontrar tu personaje:', error);
-            document.getElementById('result').innerHTML = 'Error al buscar tu personaje. Por favor, inténtalo de nuevo.';
+            console.error('Error al buscar tu personaje:', error);
+            document.getElementById('result').innerHTML = 'Error al buscar tu personajito. Por favor, inténtalo de nuevo.';
         });
 });
+
 swapi.get('https://swapi.dev/api/planets/').then((resultad) => {
     console.log(result);
     return result.nextPage();
