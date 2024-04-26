48630532c50454629ca0dd84644b943cca689709
Andre230906 <andresportillaart@gmail.com>
2024-04-26 08:46:20 -0500

    arreglo

--- DIA11/main.js
+++ DIA11/main.js
@@ -1,17 +1,22 @@
 let currentCharacter = 1;
-let lastSearchedCharacter = 1; // Variable para guardar el último Pokémon buscado
+let lastSearchedCharacter = 1;
 
 function mostrarPersonaje(numero) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('pokemonImg').src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
-            document.getElementById('pokemonName').textContent = `${data.id}-${data.name}`;
-            currentCharacter = numero;
-        })
-        .catch(error => {
-            console.error('Error al obtener los datos del personaje', error);
-        });
+    if (numero >= 650 && numero <= 1025) {
+        document.getElementById('pokemonImg').src = '';
+        document.getElementById('pokemonName').textContent = `Trabajando en la animación de este pokemon. Estará disponible pronto.`;
+    } else {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('pokemonImg').src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+                document.getElementById('pokemonName').textContent = `${data.id}-${data.name}`;
+                currentCharacter = numero;
+            })
+            .catch(error => {
+                console.error('Error al obtener los datos del personaje', error);
+            });
+    }
 }
 
 function buscarYMostrarPersonaje(searchTerm) {
@@ -48,6 +53,6 @@ document.getElementById('prevBtn').addEventListener('click', () => {
 });
 
 document.getElementById('nextBtn').addEventListener('click', () => {
-    currentCharacter = Math.min(1000, currentCharacter + 1);
+    currentCharacter = Math.min(1025, currentCharacter + 1);
     mostrarPersonaje(currentCharacter);
 });
